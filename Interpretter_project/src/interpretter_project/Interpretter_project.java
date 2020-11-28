32375491c7e0b90141b79bbea97a15cf5a82371e
Brett Eckert <bdem7v@mail.missouri.edu>
2020-11-27 20:22:31 -0600

    added function to tell whether or not there is a tab and added print function for every character to demonstrate how to see new lines and tabs

--- Interpretter_project/src/interpretter_project/Interpretter_project.java
+++ Interpretter_project/src/interpretter_project/Interpretter_project.java
@@ -21,15 +21,34 @@ public class Interpretter_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        String content=readFile("test.py", StandardCharsets.US_ASCII);
-        System.out.println(content);
+        String file=readFile("test.py", StandardCharsets.US_ASCII);
+        
+        System.out.println("Characters in file:\n");
+        for(int i=0; i<=file.length()-1; i++) {
+            if(file.charAt(i)=='\n'){
+                System.out.print("NEW LINE HERE");
+            }
+            if(checkTab(file, i)){
+                System.out.print("TAB HERE");
+            } 
+            System.out.println(file.charAt(i));
+	}
+        
+        System.out.println("\nOriginal File:\n" +file);
     }
     
-    static String readFile(String path, Charset encoding)
-    throws IOException
-    {
+    static String readFile(String path, Charset encoding) throws IOException{
       byte[] encoded = Files.readAllBytes(Paths.get(path));
       return new String(encoded, encoding);
+    }
+    
+    static boolean checkTab(String file, int i){
+        if(i>=3){
+            if(file.charAt(i)==' ' && file.charAt(i-1)==' ' && file.charAt(i-2)==' ' && file.charAt(i-3)==' '){
+                return true;
+            }
+        }
+        return false;
     }
     
 }
